e29ed6a5eb8ccc9068b531399cf206f7ed95e63d
Nimit jain <nimitj.ec.19@nitj.ac.in>
2023-10-13 11:53:44 +0530

    Update Guenum.js

--- Guenum.js
+++ Guenum.js
@@ -171,7 +171,7 @@ class Game {
 
     }
     updateCountdown() {
-        const updateHour = 24; // The hour when the update will occur (e.g., 15 for 
+        const updateHour = 12; // The hour when the update will occur (e.g., 15 for 
         function calculateTimeRemaining() {
             const now = new Date();
             const nextUpdate = new Date();
